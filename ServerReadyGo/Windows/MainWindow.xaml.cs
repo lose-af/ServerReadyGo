5b13066a81f9c08c11e1614d1f5b9d92ec63f9fa
线粒体XianlitiCN <xianliticn@outlook.com>
2021-08-05 14:05:27 +0800

    守护功能初步（未完成）

--- ServerReadyGo/Windows/MainWindow.xaml.cs
+++ ServerReadyGo/Windows/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace ServerReadyGo
 {
@@ -20,9 +23,32 @@ namespace ServerReadyGo
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker protector;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            protector = new BackgroundWorker();
+            protector.DoWork += Protector_DoWork;
+
+            protector.RunWorkerAsync(4292);
+        }
+
+        private void Protector_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int PID = (int)e.Argument;
+            Process p = Process.GetProcessById(PID);
+
+            txbProName.Text = p.ProcessName;
+            txbProID.Text = PID.ToString();
+            txbProStatus.Text = p.HasExited ? "已终止" : "正在运行";
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation ani = new DoubleAnimation(0, 1, new Duration(TimeSpan.FromSeconds(1)));
+            (sender as Grid).BeginAnimation(OpacityProperty, ani);
         }
     }
 }
